1caf7fd640c386a6f9fcff871671205b23e087ad
kamiada <kamiada528@gmail.com>
2021-08-20 14:06:08 +0100

    Divided data into chunks

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./stylesheet.scss";
-import data from './data.json';
-import Pagination from './Pagination';
+import data from "./data.json";
+import Pagination from "./Pagination";
 
-//divide data per 4
+//divide data per 3
 //get length of how many chunks of data - that's the number of pages - total number
 // get current page
 const DEFAULT_SIZE = 3;
+
 const divideData = (arr, leng) => {
-    let newArr = [];
-    if(Array.isArray(arr) ) {
-        let chunk = arr.slice(0, leng);
-        newArr.push(chunk);
+  let newArr = [];
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i++) {
+      if (i > arr.length) {
+        break;
+      }
+      let chunk = arr.slice(i, leng);
+      newArr.push(chunk);
+      leng += DEFAULT_SIZE;
+      i += DEFAULT_SIZE -1;
     }
-    console.log(newArr);
-}
+  }
+  return newArr;
+};
 const getPage = (array, howManyToShow, index) => {
-    const paginatedDate = divideData(array, howManyToShow);
-    console.log(paginatedDate);
-}
+  const paginatedData = divideData(array, howManyToShow);
+  const totalNumbOfPages = paginatedData.length;
+};
 
 class Page extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentPageIndex : 1,
-            totalNumbOfPages : 0,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPageIndex: 1,
+      totalNumbOfPages: 0,
+    };
+  }
 
-    render() {
-        return ( 
-            <div className="container">
-               {
-                getPage(data, DEFAULT_SIZE, 1)
-               }
-            </div>
-        )
-    }
+  render() {
+    return <div className="container">{getPage(data, DEFAULT_SIZE, 1)}</div>;
+  }
 }
 
 export default Page;
