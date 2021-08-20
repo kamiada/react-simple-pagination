e9d2a9ccfb3dbee56bae517eac8967fe4e06555a
kamiada <kamiada528@gmail.com>
2021-08-20 11:16:03 +0100

    dividing array and such

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Page from './components/Page';
-import Pagination from './components/Pagination';
-
 
 function App() {
   return (
     <div className="App">
         <Page />
-        
     </div>
   );
 }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import "./stylesheet.scss";
+import data from './data.json';
+import Pagination from './Pagination';
+
+//divide data per 4
+//get length of how many chunks of data - that's the number of pages - total number
+// get current page
+const DEFAULT_SIZE = 4;
+const divideData = (arr, leng) => {
+    if(arr.length > 0 ) {
+
+    }
+}
+const getPage = (array, howManyToShow, index) => {
+    const paginatedDate = divideData(array, howManyToShow);
+
+}
 
 class Page extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPageIndex : 1,
+            totalNumbOfPages : 0,
+        }
+    }
+
     render() {
         return ( 
             <div className="container">
-                
+                {this.props.data}
             </div>
         )
     }
